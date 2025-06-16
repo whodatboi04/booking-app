8b74833f07d3a48d2e212d07558a3b33ed2ca001
whodatboi04 <ryanmarkdelosreyes04@gmail.com>
2025-06-16 16:16:15 +0800

    add user factor username

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
